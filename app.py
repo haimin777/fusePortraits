f1216f7e7abf0dad1d51a4e17fb6d4f7f1df5829
haimin <med53@mail.ru>
2019-08-05 20:55:55 +0300

    app.py uploads2

--- app.py
+++ app.py
@@ -78,6 +78,31 @@ def upload():
         # return redirect(url_for('uploaded_file', filename=filename))
         return render_template('second_download.html')
 
+
+@app.route('/upload2', methods=['POST'])
+def upload2():
+
+    # Get the name of the uploaded files
+    # Get the name of the uploaded file
+    file = request.files['file']
+            # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+                # Make the filename safe, remove unsupported chars
+                filename = secure_filename(file.filename)
+                # Move the file form the temporal folder to
+                # the upload folder we setup
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # Redirect the user to the uploaded_file route, which
+                # will basicaly show on the browser the uploaded file
+                print(filename)
+                # testcv.image_make(filename)
+
+                # return redirect(url_for('uploaded_file', filename=filename))
+                #return render_template('second_download.html')
+
+                print('redirected to process file')
+                return redirect(url_for('preprocess_images'))
+
 # This route is expecting a parameter containing the name
 # of a file. Then it will locate that file on the upload
 # directory and show it on the browser, so if the user uploads
